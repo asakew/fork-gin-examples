903efe20b1562d29f6f9ef1c886dd2d06aeb3acb
Akmal Sindarov <asauz777@gmail.com>
2024-11-13 19:34:40 +1000

    fix

--- upload-file/single/main.go
+++ upload-file/single/main.go
@@ -9,9 +9,22 @@ import (
 
 func main() {
 	router := gin.Default()
+
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
-	router.Static("/", "./public")
+
+	// Serve static files from the ./public directory
+	router.Static("/public", "./public")
+
+	// Load HTML templates from the "public" directory
+	router.LoadHTMLGlob("public/*.html")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Upload a single file",
+		})
+	})
+
 	router.POST("/upload", func(c *gin.Context) {
 		name := c.PostForm("name")
 		email := c.PostForm("email")
@@ -23,13 +36,15 @@ func main() {
 			return
 		}
 
+		// Save the uploaded file
 		filename := filepath.Base(file.Filename)
-		if err := c.SaveUploadedFile(file, filename); err != nil {
+		if err := c.SaveUploadedFile(file, "./uploads/"+filename); err != nil {
 			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
 			return
 		}
 
-		c.String(http.StatusOK, "File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email)
+		c.String(http.StatusOK, "File %s uploaded successfully with fields name=%s and email=%s.", filename, name, email)
 	})
+
 	router.Run(":8080")
 }
